db959ebd840854afdb73f0b87675bca285550089
Melih Altun <maltun@k1m30s03.42istanbul.com.tr>
2023-11-09 02:02:20 +0300

    başlıooz

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: maltun <maltun@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 18:22:11 by maltun            #+#    #+#             */
-/*   Updated: 2023/11/09 01:56:13 by maltun           ###   ########.fr       */
+/*   Updated: 2023/11/09 02:02:03 by maltun           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,18 @@
 int	main(int ac, char **av, char **env)
 {
 	char	*str;
+	char	*str2;
 
 	(void)ac;
 	(void)av;
 	(void)env;
 	while (1)
 	{
-		str = readline("melih :");
-		printf("%s\n", str);
+		printf("--------------------\n");
+		str = readline("isim : ");
+		str2 = readline("soyisim : ");
+		printf("--------------------\n%s\n", str);
+		printf("%s\n", str2);
 		free(str);
 	}
 	return (0);
